bcef25cc6c057623848be296fea7e958308437fd
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-02 23:17:50 +0300

    Configurate Express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const PORT = 5000;
 
-
-
+const expressConfigurator = require('./config/expressConfiguration');
 
 const app = express();
+
+app.use(expressConfigurator);
 
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}...`));
