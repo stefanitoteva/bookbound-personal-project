592ccf4119757ca16cb8fd0d0228ae81435ce7e1
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-02 23:20:47 +0300

    Handlebars configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const PORT = 5000;
 
-const expressConfigurator = require('./config/expressConfiguration');
+const expressConfigurator = require('./config/expressConfig');
+const hbsConfigurator = require('./config/hbsConfig');
 
 const app = express();
 
 app.use(expressConfigurator);
+app.use(hbsConfigurator);
 
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}...`));
